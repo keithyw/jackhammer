13ccbf0aef93385fd5fadcc23ca8859ab5503f17
Keith Watanabe <conark@gmail.com>
2018-01-21 11:38:00 -0800

    fixed path

--- src/Providers/JackhammerServiceProvider.php
+++ src/Providers/JackhammerServiceProvider.php
@@ -20,11 +20,11 @@ class JackhammerServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'jackhammer');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'jackhammer');
         $this->publishes([
-            __DIR__ . '/config/jackhammer.php' => config_path('jackhammer.php'),
-            __DIR__ . '/config/repositories.php' => config_path('repositories.php'),
-            __DIR__ . '/resources/views/admin.blade.php' => base_path('resources/views/admin.blade.php')
+            __DIR__ . '/../config/jackhammer.php' => config_path('jackhammer.php'),
+            __DIR__ . '/../config/repositories.php' => config_path('repositories.php'),
+            __DIR__ . '/../resources/views/admin.blade.php' => base_path('resources/views/admin.blade.php')
         ]);
     }
 }
